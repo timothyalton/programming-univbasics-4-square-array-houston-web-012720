df4c285d3f85fab923590f24e55f77dbc1bac909
Mitchell T Alton <mitchell.alton@live.com>
2019-12-28 23:00:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  counter = 0 
-  while counter < array.length do
-    new_numbers = array[counter]
-    counter += 1 
+ new_array = []
+ counter = 0 
+ while counter < array.length do
+   
   end
 end
