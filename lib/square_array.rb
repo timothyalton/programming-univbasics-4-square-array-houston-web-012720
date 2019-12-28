7c7be52eb92a8ea10a005a545137dcdb9eb45e6a
Mitchell T Alton <mitchell.alton@live.com>
2019-12-28 23:04:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
  new_array = []
  counter = 0 
  while counter < array.length do
-   new_array << array([counter]**2)
+   new_array << array[counter] ** 2
    counter += 1
   end
+  return new_array
 end
